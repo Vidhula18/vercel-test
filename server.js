7df78686bff43b0e3fff393ab0686b436c25f815
Vidhula18 <vidhulaganesh@gmail.com>
2023-10-18 17:29:16 +0530

    hello added

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
+const http = require('http');
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from your Vercel server!' });
 });
 
 module.exports = app;
+
+
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
